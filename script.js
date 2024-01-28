ddcd34454a458a580d5ae89461f0b5b889457d23
Ehsansobhni <131485807+Ehsansobhni@users.noreply.github.com>
2024-01-29 00:27:45 +0330

    name and add if for stop

--- script.js
+++ script.js
@@ -2,23 +2,29 @@ let hour = 0;
 let minute = 0;
 let second = 0;
 let time;
+let isStopped = false; // Add this line
 
-
-function starTimer() {
-    // stopTimer();
+function startTimer() {
+    isStopped = false;
     document.getElementById("start").setAttribute("disabled","true");
     time = setInterval(() => { timer(); }, 1000);
-  }
-  
-  function stopTimer() {
+
+
+}
+
+
+
+function stopTimer() {
+  if(isStopped) { // Add this condition
+    return;
+}
+    isStopped = true; // Set isStopped to true when the timer is stopped
     document.getElementById("start").removeAttribute("disabled");
     clearInterval(time);
-    lip(hour,minute,second);
-    
-    
-  }
-  
-  function reseTimer() {
+    lap(hour,minute,second);
+}
+
+function resetTimer() {
     stopTimer();
     hour = 0;
     minute = 0;
@@ -27,11 +33,14 @@ function starTimer() {
     document.getElementById('hour').innerText = '00';
     document.getElementById('min').innerText = '00';
     document.getElementById('sec').innerText = '00';
-    
-  }
-  function timer() {
+}
 
-      second++;
+function timer() {
+    if(isStopped) { // Add this condition
+        return;
+    }
+
+    second++;
     
     if (second == 60) {
       second = 0;
@@ -42,25 +51,26 @@ function starTimer() {
       hour++;
     }
     if (hour == 24){
-        reseTimer(); 
+        resetTimer(); 
     }
     document.getElementById('hour').innerText = returnData(hour);
     document.getElementById('min').innerText = returnData(minute);
     document.getElementById('sec').innerText = returnData(second);
-    
-  }
-  
-  function returnData(input) {
+}
+
+function returnData(input) {
     return input > 9 ? input : `0${input}`
-  }
+}
 
-  function lap(h,m,s){
+function lap(h,m,s){
+    // If all are zero, don't add a lap
+    if(h === 0 && m === 0 && s === 0) {
+        return;
+    }
+
     const tableLap = document.querySelector("#laps");
-    
     const timerDate = document.createElement("h2");
-    
-
     const timerNode = document.createTextNode(returnData(h)+":"+ returnData(m)+":"+returnData(s));
     timerDate.appendChild(timerNode);
     tableLap.appendChild(timerDate);
-  }
+}
